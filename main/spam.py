cb7c4de14cbea5745e679e42e458daa47073b46c
Max <Max.S.1988@hotmail.com>
2019-11-25 04:19:18 +0100

    update

--- main/spam.py
+++ main/spam.py
@@ -8,6 +8,7 @@ import sys
 import os
 
 # Colour Scheme
+
 class bcolors:
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
@@ -15,8 +16,8 @@ class bcolors:
     ENDC = '\033[0m'
 
 # Functions
+
 def banner():
-    try:
     os.system('clear')
     try:
         file1 = open('banner.txt', 'r')
@@ -25,6 +26,8 @@ def banner():
         file1.close()
     except IOError:
         print('Banner File not found')
+
+# Choose Mail Service
 
 def mailChoice():
     print(bcolors.WARNING + '''
@@ -36,141 +39,153 @@ def mailChoice():
     choice = input(bcolors.OKGREEN + '\nNumber: ' + bcolors.ENDC)
     return choice
 
+# Gmail DISCLAIMER
+
 def gmailInstruct():
-    print(bcolors.FAIL + "DISCLAIMER: Gmail has a limit of 500 emails per day per account" + bcolors.ENDC)
-    print(bcolors.WARNING + """\nThis can be surpassed by using multiple emails.
-    Do you wish to surpass the limit using multiple emails? (Emails and passwords must be predefined in
-    gmail.txt and gmailpass.txt)
-    1) Yes
-    2) No
-    """ + bcolors.ENDC)
+    print(bcolors.FAIL + "\nDISCLAIMER: Gmail has a limit of 500 emails per day per account" + bcolors.ENDC)
+    print(bcolors.WARNING + "This can be surpassed by using multiple emails."\
+    "\nDo you wish to surpass the limit using multiple emails?"\
+    "\n(Emails and passwords must be predefined in gmail.txt and gmailpass.txt)"\
+    "\n1) Yes"\
+    "\n2) No"\
+    + bcolors.ENDC)
     multiple = input(bcolors.OKGREEN + '\nNumber: ' + bcolors.ENDC)
     return multiple
+
+# Yahoo DISCLAIMER
 
 def yahooInstruct():
-    print(bcolors.FAIL + "DISCLAIMER: Yahoo has a limit of 100 emails per hour per account" + bcolors.ENDC)
-    print(bcolors.WARNING + """\nThis can be surpassed by using multiple emails.
-    Do you wish to surpass the limit using multiple emails? (Emails and passwords must be predefined in
-    yahoo.txt and yahoopass.txt)
-    1) Yes
-    2) No
-    """ + bcolors.ENDC)
+    print(bcolors.FAIL + "\nDISCLAIMER: Yahoo has a limit of 100 emails per hour per account" + bcolors.ENDC)
+    print(bcolors.WARNING + "This can be surpassed by using multiple emails."\
+    "\nDo you wish to surpass the limit using multiple emails?"\
+    "\n(Emails and passwords must be predefined in yahoo.txt and yahoopass.txt)"\
+    "\n1) Yes"\
+    "\n2) No"\
+    + bcolors.ENDC)
     multiple = input(bcolors.OKGREEN + '\nNumber: ' + bcolors.ENDC)
     return multiple
+
+# Outlook DISCLAIMER
 
 def outlookInstruct():
-    print(bcolors.FAIL + "DISCLAIMER: Outlook/Hotmail has a limit of 100 recipiants per day per account" + bcolors.ENDC)
-    print(bcolors.WARNING + """\nThis can be surpassed by using multiple emails.
-    Do you wish to surpass the limit using multiple emails? (Emails and passwords must be predefined in
-    outlook.txt and outlookpass.txt)
-    1) Yes
-    2) No
-    """ + bcolors.ENDC)
+    print(bcolors.FAIL + "\nDISCLAIMER: Outlook/Hotmail has a limit of 100 recipiants per day per account" + bcolors.ENDC)
+    print(bcolors.WARNING + "This can be surpassed by using multiple emails."\
+    "\nDo you wish to surpass the limit using multiple emails?"\
+    "\n(Emails and passwords must be predefined in outlook.txt and outlookpass.txt)"\
+    "\n1) Yes"\
+    "\n2) No"\
++ bcolors.ENDC)
     multiple = input(bcolors.OKGREEN + '\nNumber: ' + bcolors.ENDC)
     return multiple
 
-#if multiple == "1" or multiple.upper() == "YES":
+# Surpass Limit with multiple emails (Gmail)
+
 def gMultiple():
+    global emailnum
+    global passnum
     try:
         file = open("gmail.txt", "r")
         fileStuff = file.readline()
         gmail = fileStuff.split(",")
-        emailnum = -1
-        if gmail[emailnum] != "\n":
-            emailnum += 1
-            from_addr = gmail[emailnum]
-        else:
-            print (bcolors.FAIL + "ERROR: No more emails in gmail.txt" + bcolors.ENDC)
+        print (gmail)
+        print (gmail[1])
+        print (emailnum)
+        emailnum += 1
+        print (emailnum)
+        from_addr = gmail[emailnum]
         file.close()
     except IOError:
         print ("gmail.txt not found")
+        from_addr = "\n"
     try:
         passFile = open("gmailpass.txt", "r")
         passFileStuff = passFile.readline()
-        passThing = passfileStuff.split(",")
-        passnum = -1
-        if passThing[passnum] != "\n":
-            passnum += 1
-            cipher = passThing[passnum]
-        else:
-            print (bcolors.FAIL + "ERROR: No more passwords in gmailpass.txt" + bcolors.ENDC)
+        passThing = passFileStuff.split(",")
+        passnum += 1
+        cipher = passThing[passnum]
         file.close()
     except IOError:
         print ("gmailpass.txt not found")
+        cipher = "\n"
+
     return from_addr,cipher
 
+# Surpass Limit with multiple emails (Yahoo)
+
 def yMultiple():
+    global emailnum
+    global passnum
     try:
         file = open("yahoo.txt", "r")
         fileStuff = file.readline()
         yahoo = fileStuff.split(",")
-        emailnum = -1
-        if yahoo[emailnum] != "\n":
-            emailnum += 1
-            from_addr = yahoo[emailnum]
-        else:
-            print (bcolors.FAIL + "ERROR: No more emails in yahoo.txt" + bcolors.ENDC)
+        emailnum += 1
+        from_addr = yahoo[emailnum]
         file.close()
     except IOError:
         print ("yahoo.txt not found")
+        from_addr = "\n"
     try:
         passFile = open("yahoopass.txt", "r")
         passFileStuff = passFile.readline()
-        passThing = passfileStuff.split(",")
-        passnum = -1
-        if passThing[passnum] != "\n":
-            passnum += 1
-            cipher = passThing[passnum]
-        else:
-            print (bcolors.FAIL + "ERROR: No more passwords in yahoopass.txt" + bcolors.ENDC)
+        passThing = passFileStuff.split(",")
+        passnum += 1
+        cipher = passThing[passnum]
         file.close()
     except IOError:
         print ("yahoopass.txt not found")
+        cipher = "\n"
     return from_addr,cipher
 
+# Surpass Limit with multiple emails (Outlook/Hotmail)
+
 def oMultiple():
+    global emailnum
+    global passnum
     try:
         file = open("outlook.txt", "r")
         fileStuff = file.readline()
         outlook = fileStuff.split(",")
-        emailnum = -1
-        if outlook[emailnum] != "\n":
-            emailnum += 1
-            from_addr = outlook[emailnum]
-        else:
-            print (bcolors.FAIL + "ERROR: No more emails in outlook.txt" + bcolors.ENDC)
+        emailnum += 1
+        from_addr = outlook[emailnum]
         file.close()
     except IOError:
         print ("outlook.txt not found")
+        from_addr = "\n"
     try:
         passFile = open("outlookpass.txt", "r")
         passFileStuff = passFile.readline()
-        passThing = passfileStuff.split(",")
-        passnum = -1
-        if passThing[passnum] != "\n":
-            passnum += 1
-            cipher = passThing[passnum]
-        else:
-            print (bcolors.FAIL + "ERROR: No more passwords in outlookpass.txt" + bcolors.ENDC)
+        passThing = passFileStuff.split(",")
+        passnum += 1
+        cipher = passThing[passnum]
         file.close()
     except IOError:
         print ("outlookpass.txt not found")
+        cipher = "\n"
     return from_addr,cipher
+
+# Send with limits (Gmail)
 
 def gSingle():
     from_addr = input(bcolors.OKGREEN + 'Your Google Email: ' + bcolors.ENDC)
     cipher = getpass.getpass(bcolors.OKGREEN + 'Password:' + bcolors.ENDC)
     return from_addr,cipher
 
+# Send with limits (Yahoo)
+
 def ySingle():
     from_addr = input(bcolors.OKGREEN + 'Your Yahoo Email: ' + bcolors.ENDC)
     cipher = getpass.getpass(bcolors.OKGREEN + 'Password:' + bcolors.ENDC)
     return from_addr,cipher
 
+# Send with limits (Outlook/Hotmail)
+
 def oSingle():
     from_addr = input(bcolors.OKGREEN + 'Your Hotmail/Outlook Email: ' + bcolors.ENDC)
     cipher = getpass.getpass(bcolors.OKGREEN + 'Password:' + bcolors.ENDC)
     return from_addr,cipher
+
+# Main structure (subject, body, etc.)
 
 def structure():
     to_addr = []
@@ -210,9 +225,12 @@ def structure():
     time.sleep(1)
     return speed,to_addr,body,subject
 
+# Main Spammer (Gmail)
+
 def gmailSpam(speed,from_addr,to_addr,body,subject,length,cipher):
     while True:
-            global sent += 1
+            global sent
+            sent += 1
             number = random.randint(0, 10000)
             subject = subject[0:length] + " (" + str(number) + ")"
             # Construct email
@@ -238,9 +256,12 @@ def gmailSpam(speed,from_addr,to_addr,body,subject,length,cipher):
                 print(bcolors.FAIL + "\nThe The Recipient's email adress is invalid! Exiting..." + bcolors.ENDC)
                 sys.exit()
 
+# Main Spammer (Yahoo)
+
 def yahooSpam(speed,from_addr,to_addr,body,subject,length,cipher):
     while True:
-        global sent += 1
+        global sent
+        sent += 1
         number = random.randint(0, 10000)
         subject = subject[0:length] + " (" + str(number) + ")"
         # Construct email
@@ -266,9 +287,12 @@ def yahooSpam(speed,from_addr,to_addr,body,subject,length,cipher):
             print(bcolors.FAIL + "\nThe The Recipient's email adress is invalid! Exiting..." + bcolors.ENDC)
             sys.exit()
 
+# Main Spammer (Outlook/Hotmail)
+
 def outlookSpam(speed,from_addr,to_addr,body,subject,length,cipher):
     while True:
-        global sent += 1
+        global sent
+        sent += 1
         number = random.randint(0, 10000)
         subject = subject[0:length] + " (" + str(number) + ")"
         # Construct email
@@ -296,3 +320,32 @@ def outlookSpam(speed,from_addr,to_addr,body,subject,length,cipher):
 
 # Main Program
 sent = 0
+emailnum = -1
+passnum = -1
+banner()
+choice = mailChoice()
+# Gmail
+if choice == "1":
+    multiple = gmailInstruct()
+    if multiple == "1" or multiple.upper() == "YES":
+        from_address,password = gMultiple()
+        spam = True
+        while spam is True:
+            print(from_address,"\n",password)
+            sent += 1
+            #gmailSpam(from_addr,cipher)
+            if sent == 19:
+                sent = 0
+                from_address,password = gMultiple()
+                spam = True
+                if from_address or password != "\n":
+                    spam = False
+    elif multiple == "2" or multiple == "NO":
+        from_address,password = gSingle()
+        sent += 1
+        while sent < 19:
+            #gmailSpam(from_address,password)
+            print(f"{from_address}, {password}")
+    else:
+        print (bcolors.FAIL + "Invaid choice!" + bcolors.ENDC)
+        sys.exit()
