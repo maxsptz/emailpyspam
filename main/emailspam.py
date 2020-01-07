b93fc0117e9d819589f95456147b753e8c2392f4
NONAME1103 <57779612+NONAME1103@users.noreply.github.com>
2020-01-07 23:22:06 +0100

    Update emailspam.py

--- main/emailspam.py
+++ main/emailspam.py
@@ -63,6 +63,29 @@ class bcolors:
     REDBG2    = '\33[101m'
 
 # Functions
+def resize():
+    cmdsize = os.get_terminal_size()
+    num = 0
+    col = ""
+    lin = ""
+    for c in range (len (cmdsize)):
+        try:
+            if cmdsize[c] == "=":
+                num += 1
+            int (cmdsize[c])
+        except ValueError:
+            "Placeholder"
+        else:
+            if num == 1:
+                col += str (cmdsize[c])
+            else:
+                lin += str (cmdsize[c])
+    if sys.platform.startswith('win32'):
+        if col < "119" or lin < "35":
+            os.system('mode con:cols=119 lines=35')
+    else:
+        if col < "119" or lin < "49":
+            os.system("printf '\e[8;49;119t'")
 
 def banner():
     if sys.platform.startswith('win32'):
@@ -99,35 +122,31 @@ def validMultiple(mult):
             mult = input(bcolors.FAIL + "Enter a valid choice: " + bcolors.ENDC)
     return mult
 
-def validRecipientNum(to_addr,recipientNum,choice):
+def validRecipientNum(to_addr,recipientNum):
     valid = False
     while not valid:
         message = False
-        if recipientNum > 0:
-            if choice == "1":
-                if recipientNum <= 500:
-                    valid = True
-                else:
-                    message = True
-            elif choice == "2" or choice == "3":
-                if recipientNum <= 100:
-                    valid = True
-                else:
-                    message = True
+        if 0 < recipientNum <= 500:
+            valid = True
         else:
             message = True
         if message:
             print (bcolors.FAIL + "Invalid number of recipients! You must start over.\n" + bcolors.ENDC)
             time.sleep(0.5)
             to_addr = []
+            addr = ""
+            number = random.randint(0, 10000)
             while True:
-                addr = input(bcolors.OKGREEN + "Type in the recipient(s), hit enter to finish: " + bcolors.ENDC)
-                if not addr:
+                addr = input(bcolors.OKGREEN + "Type in the recipient(s), hit enter to finish (or \"#\" to use a predefined recipient list): " + bcolors.ENDC)
+                if addr == "#":
+                    recipients = listSelector(recipientLists)
+                    if recipients != "normal":
+                        to_addr = recipients
+                        break
+                elif not addr:
                     break
                 else:
                     to_addr.append(addr)
-            recipientNum = len (to_addr)
-            valid = False
     return to_addr,recipientNum
 
 def validSend(send,multiple,recipientNum,numOfSenders):
@@ -191,12 +210,180 @@ def validGmail(from_addr,cipher):
         valid = True
     return valid
 
+def saveRescipientLists():
+    f = open ("recipientLists.txt", "w")
+    for thing in range (len (sorted (recipientLists))):
+        f.write (sorted (recipientLists)[thing] + ":")
+        for things in range (len (recipientLists[sorted (recipientLists)[thing]])):
+            f.write (recipientLists[sorted (recipientLists)[thing]][things] + ":")
+        f.write ("\n")
+    f.close()
 
-def recipientLists():
-    print("LOL")
+def recipientEditor(recipientLists):
+    print(bcolors.WARNING + '''
+    Choose an Option:
+    1) Add a list of recipients
+    2) Edit list of recipients
+    3) Delete a list of recipients
+    4) View all lists
+    5) Quit recipient editor
+    ''' + bcolors.ENDC + '--------------------------------------------------------------')
+    option = input(bcolors.OKGREEN + '\nNumber: ' + bcolors.ENDC)
+
+    while option != "5":
+        if option == "1":
+            nameList = input(bcolors.OKGREEN + "\nName of new list: " + bcolors.ENDC)
+            if nameList in recipientLists:
+                print (bcolors.FAIL + "\nList already exists!\n" + bcolors.ENDC)
+            else:
+                rList = []
+                while True:
+                    entry = input(bcolors.OKGREEN + "Type in the recipient(s), hit enter to finish: " + bcolors.ENDC)
+                    if not entry:
+                        break
+                    else:
+                        rList.append(entry)
+                print ("\n" + bcolors.OKGREEN + nameList + ":",end = " ")
+                print (rList)
+                print ("" + bcolors.ENDC,end = "")
+                sure = input(bcolors.REDBG + "\nAre you sure? (Y/N): " + bcolors.ENDC)
+                if sure.upper() == "Y":
+                    recipientLists[nameList] = rList
+                    saveRescipientLists()
+                else:
+                    break
+        elif option == "2":
+            if len (recipientLists) == 0:
+                print (bcolors.FAIL + "\nYou don't have any saved lists yet!\n" + bcolors.ENDC)
+            else:
+                wantEdit = input(bcolors.OKGREEN + '\nEnter the name of the list you would like to edit: ' + bcolors.ENDC)
+                if wantEdit not in recipientLists:
+                    print (bcolors.FAIL + "\nList not found!\n" + bcolors.ENDC)
+                else:
+                    print ("\n" + bcolors.OKGREEN + wantEdit + ":",end = " ")
+                    print (recipientLists[wantEdit])
+                    print ("" + bcolors.ENDC,end = "")
+                    print(bcolors.WARNING + '''
+                    Choose an Option:
+                    1) Add a recipient
+                    2) Delete a recipient
+                    3) Exit
+                    ''' + bcolors.ENDC + '--------------------------------------------------------------')
+                    opt = input (bcolors.OKGREEN + "\nNumber: " + bcolors.ENDC)
+                    while opt != "3":
+                        if opt == "1":
+                            edit = input (bcolors.OKGREEN + "\nEnter new recipient: " + bcolors.ENDC)
+                            rlist = recipientLists[wantEdit]
+                            if edit in rlist:
+                                print (bcolors.FAIL + "\nRecipient already in list!\n" + bcolors.ENDC)
+                            else:
+                                rlist.append (edit)
+                                recipientLists[wantEdit] = rlist
+                                print (bcolors.OKGREEN + '\nCurrent list:\n')
+                                print (recipientLists[wantEdit])
+                                print ("" + bcolors.ENDC,end = "")
+                                saveRescipientLists()
+                        elif opt == "2":
+                            if len (recipientLists[wantEdit]) == 0:
+                                print (bcolors.FAIL + "\nYou don't have any recipients in this list yet!\n" + bcolors.ENDC)
+                            else:
+                                edit = input (bcolors.OKGREEN + "\nEnter recipient you wish to delete: " + bcolors.ENDC)
+                                rlist = recipientLists[wantEdit]
+                                if edit not in rlist:
+                                    print (bcolors.FAIL + "\nRecipient not found!\n" + bcolors.ENDC)
+                                else:
+                                    rlist.remove (edit)
+                                    recipientLists[wantEdit] = rlist
+                                print (bcolors.OKGREEN + '\nCurrent list:\n')
+                                print (recipientLists[wantEdit])
+                                print ("" + bcolors.ENDC,end = "")
+                                saveRescipientLists()
+                        else:
+                            print (bcolors.FAIL + "\nInvaid choice!\n" + bcolors.ENDC)
+                        print(bcolors.WARNING + '''
+                        Choose an Option:
+                        1) Add a recipient
+                        2) Delete a recipient
+                        3) Exit
+                        ''' + bcolors.ENDC + '--------------------------------------------------------------')
+                        opt = input (bcolors.OKGREEN + "\nNumber: " + bcolors.ENDC)
+        elif option == "3":
+            if len (recipientLists) == 0:
+                    print (bcolors.FAIL + "\nYou don't have any saved lists yet!\n" + bcolors.ENDC)
+            else:
+                wantDel = input (bcolors.OKGREEN + "\nEnter the name of the list you would like to delete: " + bcolors.ENDC)
+                if wantDel not in recipientLists:
+                    print (bcolors.FAIL + "\nList not found" + bcolors.ENDC)
+                else:
+                    print ("\n" + bcolors.OKGREEN + wantDel + ":",end = " ")
+                    print (recipientLists[wantDel])
+                    print ("" + bcolors.ENDC,end = "")
+                    sure = input(bcolors.REDBG + "\nAre you sure? (Y/N): " + bcolors.ENDC)
+                    if sure.upper() == "Y":
+                        del recipientLists[wantDel]
+                        saveRescipientLists()
+                    else:
+                        break
+        elif option == "4":
+            if len (recipientLists) == 0:
+                    print (bcolors.FAIL + "\nYou don't have any saved lists yet!\n" + bcolors.ENDC)
+            else:
+                keys = sorted (recipientLists)
+                for list in range (len (keys)):
+                    print ("")
+                    k = keys[list]
+                    print (bcolors.OKGREEN + k,"\n-",end = "" )
+                    print (("-")*len (k),end = "")
+                    print ("-" + bcolors.ENDC)
+                    for entry in range (len (recipientLists[k])):
+                        print (bcolors.OKGREEN + recipientLists[k][entry] + bcolors.ENDC)
+                time.sleep(1.5)
+        else:
+            print (bcolors.FAIL + "\nInvaid choice!\n" + bcolors.ENDC)
+        time.sleep(0.5)
+        print(bcolors.WARNING + '''
+        Choose an Option:
+        1) Add a list of recipients
+        2) Edit list of recipients
+        3) Delete a list of recipients
+        4) View all lists
+        5) Quit recipient editor
+        ''' + bcolors.ENDC + '--------------------------------------------------------------')
+        option = input(bcolors.OKGREEN + '\nNumber: ' + bcolors.ENDC)
+    return recipientLists
+
+def listSelector(recipientLists):
+    if len (recipientLists) == 0:
+            print (bcolors.FAIL + "\nYou don't have any saved lists yet!\n" + bcolors.ENDC)
+    else:
+        wantUse = input (bcolors.OKGREEN + "\nEnter the name of the list you would like to use (or \"#\" to show all existing lists): " + bcolors.ENDC)
+        while wantUse == "#":
+            keys = sorted (recipientLists)
+            for list in range (len (keys)):
+                print ("")
+                k = keys[list]
+                print (bcolors.OKGREEN + k,"\n-",end = "" )
+                print (("-")*len (k),end = "")
+                print ("-" + bcolors.ENDC)
+                for entry in range (len (recipientLists[k])):
+                    print (bcolors.OKGREEN + recipientLists[k][entry] + bcolors.ENDC)
+            wantUse = input (bcolors.OKGREEN + "\nEnter the name of the list you would like to use (or \"#\" to show all existing lists): " + bcolors.ENDC)
+        if wantUse not in recipientLists:
+            print (bcolors.FAIL + "\nList not found" + bcolors.ENDC)
+            recipients = "normal"
+        else:
+            print ("\n" + bcolors.OKGREEN + wantUse + ":",end = " ")
+            print (recipientLists[wantUse])
+            print ("" + bcolors.ENDC,end = "")
+            sure = input (bcolors.REDBG + "\nAre you sure? (Y/N): ")
+            print(bcolors.ENDC + "", end = "")
+            if sure.upper() == "Y":
+                recipients = recipientLists[wantUse]
+            else:
+                recipients = "normal"
+    return recipients
 
 # Choose Mail Service
-
 def mailChoice():
     print(bcolors.WARNING + '''
     Choose an Option:
@@ -208,16 +395,15 @@ def mailChoice():
     choice = input(bcolors.OKGREEN + '\nNumber: ' + bcolors.ENDC)
     return choice
 
-def legacy(choice):
-    if choice == "2" or choice == "3":
-        print(bcolors.FAIL + bcolors.BOLD + 'The development team has decided to depreciate this feature as we have run into too many errors with this mail service.' + bcolors.ENDC)
-        print(bcolors.FAIL + bcolors.BOLD + "If you would like to try and fix the errors or test these mail services, do the following:"\
-         '\n • Refer to our "legacy" branch' + bcolors.ENDC, end = " (")
-        print(bcolors.URL + "https://github.com/Curioo/emailpyspam/tree/legacy" + bcolors.ENDC, end = ")\n")
-        print(bcolors.FAIL + bcolors.BOLD + ' • Access the original code for this service'\
-        '\n(• Submit a pull request)'+ bcolors.ENDC)
-        time.sleep(1)
-        sys.exit()
+def legacy():
+    print(bcolors.FAIL + bcolors.BOLD + 'The development team has decided to depreciate this feature as we have run into too many errors with this mail service.' + bcolors.ENDC)
+    print(bcolors.FAIL + bcolors.BOLD + "If you would like to try and fix the errors or test these mail services, do the following:"\
+     '\n • Refer to our "legacy" branch' + bcolors.ENDC, end = " (")
+    print(bcolors.URL + "https://github.com/Curioo/emailpyspam/tree/legacy" + bcolors.ENDC, end = ")\n")
+    print(bcolors.FAIL + bcolors.BOLD + ' • Access the original code for this service'\
+    '\n(• Submit a pull request)'+ bcolors.ENDC)
+    time.sleep(1)
+    sys.exit()
 
 # Gmail DISCLAIMER
 
@@ -265,7 +451,6 @@ def gMultiple():
     except IOError:
         print (bcolors.FAIL + "gmailpass.txt not found! Exiting..." + bcolors.ENDC)
         sys.exit()
-
     return from_addr,cipher,numOfSenders
 
 # Send with limits (Gmail)
@@ -281,23 +466,41 @@ def gSingle():
 
 # Main structure (subject, body, etc.)
 
-def structure(choice,numOfSenders):
+def structure(numOfSenders,recipientLists):
     to_addr = []
     addr = ""
     number = random.randint(0, 10000)
     while True:
-        addr = input(bcolors.OKGREEN + "Type in the recipient(s), hit enter to finish: " + bcolors.ENDC)
-        if not addr:
+        addr = input(bcolors.OKGREEN + "Type in the recipient(s), hit enter to finish (or \"#\" to use a predefined recipient list): " + bcolors.ENDC)
+        if addr == "#":
+            recipients = listSelector(recipientLists)
+            if recipients != "normal":
+                to_addr = recipients
+                break
+        elif not addr:
+            save = input (bcolors.OKGREEN + "\nDo you want to save these recipients to a new recipient list? (Y/N): " + bcolors.ENDC)
+            if save.upper() == "Y":
+                nameList = input (bcolors.OKGREEN + "\nName of new list: " + bcolors.ENDC)
+                if nameList in recipientLists:
+                    print (bcolors.FAIL + "\nList already exists!\n" + bcolors.ENDC)
+                else:
+                    print ("\n" + bcolors.OKGREEN + nameList + ":",end = " ")
+                    print (to_addr)
+                    print ("" + bcolors.ENDC,end = "")
+                    sure = input(bcolors.REDBG + "\nAre you sure? (Y/N): " + bcolors.ENDC)
+                    if sure.upper() == "Y":
+                        recipientLists[nameList] = to_addr
+                        saveRescipientLists()
             break
         else:
             to_addr.append(addr)
     recipientNum = len (to_addr)
-    to_addr,recipientNum = validRecipientNum(to_addr,recipientNum,choice)
+    to_addr,recipientNum = validRecipientNum(to_addr,recipientNum)
     print(bcolors.FAIL + "\nKeep in mind, each recipient of the same email adds to the email count." + bcolors.ENDC)
     limit = input(bcolors.OKGREEN + "Would you like to send a specific number of emails? (Y/N): " + bcolors.ENDC)
     if limit.lower() == "y":
         send = input(bcolors.FAIL + "Enter the number of emails you want to send: " + bcolors.ENDC)
-        send = validSend(send,choice,multiple,recipientNum,numOfSenders)
+        send = validSend(send,multiple,recipientNum,numOfSenders)
     else:
         send = float ("inf")
     predef = input(bcolors.OKGREEN + 'Would you like to use the subject saved in subject.txt? (Y/N): ' + bcolors.ENDC)
@@ -374,20 +577,40 @@ try:
     Sent = 0
     emailnum = -1
     passnum = -1
+    resize()
     banner()
     choice = mailChoice()
     choice = validChoice(choice)
-    legacy(choice)
-    if choice == "#":
-        recipientLists()
-
+    try:
+        recipientLists = {}
+        f = open("recipientLists.txt", "r")
+        for l in f:
+            line = l.split(":")
+            line.remove ("\n")
+            values = []
+            key = line[0]
+            line.remove (key)
+            for v in range (len (line)):
+                val = line[v]
+                values.append (val)
+            recipientLists[key] = values
+        f.close()
+    except IOError:
+        recipientLists = {}
+        saveRescipientLists()
+    while choice == "#":
+        recipientLists = recipientEditor(recipientLists)
+        choice = mailChoice()
+        choice = validChoice(choice)
+    if choice == "2" or choice == "3":
+        legacy()
     # Gmail
     if choice == "1":
         multiple = gmailInstruct()
         multiple = validMultiple(multiple)
         if multiple == "1" or multiple.upper() == "YES":
             from_address,password,numOfSenders = gMultiple()
-            sendSpeed,to_address,body,subject,length,recipientNum,send = structure(choice,numOfSenders)
+            sendSpeed,to_address,body,subject,length,recipientNum,send = structure(numOfSenders,recipientLists)
             if loadingBar and send != float ("inf"):
                 pbar = tqdm(total=(send/recipientNum))
             elif table and recipientNum <= 2:
@@ -425,7 +648,7 @@ try:
                 pbar.close()
         elif multiple == "2" or multiple.upper() == "NO":
             from_address,password,numOfSenders = gSingle()
-            sendSpeed,to_address,body,subject,length,recipientNum,send = structure(choice,numOfSenders)
+            sendSpeed,to_address,body,subject,length,recipientNum,send = structure(numOfSenders,recipientLists)
             if loadingBar and send != float ("inf"):
                 pbar = tqdm(total=(send/recipientNum))
             elif table and recipientNum <= 2:
@@ -452,8 +675,7 @@ try:
         else:
             print (bcolors.FAIL + "Invaid choice!" + bcolors.ENDC)
             sys.exit()
+
 except KeyboardInterrupt:
     print(bcolors.FAIL + "\nCancelled!" + bcolors.ENDC)
     sys.exit()
-
-# Add an option to give a submenu to user to choose from predefined recipient list
